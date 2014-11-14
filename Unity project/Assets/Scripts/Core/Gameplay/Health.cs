a64d559cae83ae8dc3cb8504f2eac7422096c016
EpicAglet <DamienCrielaard@gmail.com>
2014-11-14 15:47:22 +0100

    Camera etc

---
+++ Unity project/Assets/Scripts/Core/Gameplay/Health.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public float InitialHP = 10;
+	public bool DestroyOnDeath = false;
+	private float hp;
+	public float HP{ get {return hp;} }
+
+	void Awake(){
+		hp = InitialHP;
+	}
+
+	void Update(){
+		if (hp <= 0){
+			if(DestroyOnDeath)
+				Destroy(gameObject);
+		}
+	}
+
+	public void Damage(float amount){
+		hp -= amount;
+	}
+
+	public void Heal(float amount){
+		Damage(-amount);
+	}
+}
---
+++ Unity project/Assets/Scripts/Core/Player/FireController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireController : MonoBehaviour {
+
+	public Transform aimTarget;
+	public Transform camera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1"))
+			OnFire();
+	}
+
+	void OnFire () {
+		Screen.lockCursor = true;
+		Vector3 dir = aimTarget.position-camera.position;
+		dir.Normalize();
+		Ray ray = new Ray(camera.position, dir);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, 1000)) {
+
+			Debug.Log("Raycast hit " + hit.transform.name);
+
+			Health hp = hit.transform.GetComponent<Health>();
+			if(hp!=null)
+				hp.Damage(1);
+		}
+	}
+}
