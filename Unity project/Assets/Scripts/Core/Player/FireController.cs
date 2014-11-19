b7498cf0fc293ef64e1ccde252904c5f40a77312
EpicAglet <DamienCrielaard@gmail.com>
2014-11-19 15:37:55 +0100

    Added basic block placement

--- Unity project/Assets/Scripts/Core/Player/FireController.cs
+++ Unity project/Assets/Scripts/Core/Player/FireController.cs
@@ -5,14 +5,36 @@ public class FireController : MonoBehaviour {
 
 	public Transform aimTarget;
 	public Transform camera;
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetButtonDown("Fire1"))
-			OnFire();
+	public Transform transBlock;
+	public Transform blockManager;
+
+	private bool blockMode = false;
+	private WorldBlockManagement blocks;
+
+	void Start(){
+		blocks = blockManager.GetComponent<WorldBlockManagement>();
 	}
 
-	void OnFire () {
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Modifier")){ //Place block mode enabled
+			blockMode = true;
+		} else {
+			blockMode = false;
+		}
+
+		if(Input.GetButtonDown("Fire1")){
+			if (blockMode){
+				OnPlaceBlock();
+			} else{
+				OnFireWeapon();
+			}
+		}
+
+		UpdateBlockOutline();
+	}
+
+	void OnFireWeapon () {
 		Screen.lockCursor = true;
 		Vector3 dir = aimTarget.position-camera.position;
 		dir.Normalize();
@@ -25,6 +47,32 @@ public class FireController : MonoBehaviour {
 			Health hp = hit.transform.GetComponent<Health>();
 			if(hp!=null)
 				hp.Damage(1);
+		}
+	}
+
+	void OnPlaceBlock(){
+		blocks.setBlockAt(Mathf.FloorToInt(transBlock.position.x),
+		                  Mathf.FloorToInt(transBlock.position.y),
+		                  Mathf.FloorToInt(transBlock.position.z),
+		                  (byte)1);
+	}
+
+	void UpdateBlockOutline(){
+		transBlock.gameObject.SetActive(blockMode);
+		if(blockMode){
+			Vector3 dir = aimTarget.position-camera.position;
+			dir.Normalize();
+			Ray ray = new Ray(camera.position, dir);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, 10)) {
+
+				Debug.DrawRay(camera.position, dir, Color.red);
+
+				transBlock.position = (new Vector3(Mathf.Floor(hit.point.x+0.5f*hit.normal.x),
+				                                   Mathf.Floor(hit.point.y+0.5f*hit.normal.y),
+				                                   Mathf.Floor(hit.point.z+0.5f*hit.normal.z))
+				                       ) + (new Vector3(0.5f, 0.5f, 0.5f));
+			}
 		}
 	}
 }
