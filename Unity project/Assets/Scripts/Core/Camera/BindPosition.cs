d017539c78bc69b3f9b83b8331194d7ac1d51b91
EpicAglet <DamienCrielaard@gmail.com>
2014-11-26 16:22:17 +0100

    fancy teddy

---
+++ Unity project/Assets/Scripts/Core/Camera/BindPosition.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BindPosition : MonoBehaviour {
+
+	public Transform toBindTo;
+	Vector3 offset;
+	Quaternion rotOffset;
+
+	// Use this for initialization
+	void Start () {
+		offset = toBindTo.position - transform.position;
+		rotOffset = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = toBindTo.position;
+		transform.rotation = toBindTo.rotation*rotOffset;
+	}
+}
