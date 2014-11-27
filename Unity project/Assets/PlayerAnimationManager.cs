0eac6ef26e0a7780f7b41983760a63351c756649
EpicAglet <DamienCrielaard@gmail.com>
2014-11-27 12:22:26 +0100

    Start animations

---
+++ Unity project/Assets/PlayerAnimationManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimationManager : MonoBehaviour {
+
+	public Transform playerMesh;
+
+	private Animation anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = playerMesh.GetComponent<Animation>();
+		anim["Run"].speed = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (anim!=null){
+			if(Input.GetKeyDown(KeyCode.Keypad9)){
+				anim.Play("Idle");
+			}
+			if(Input.GetKeyDown(KeyCode.Keypad8))
+				anim.Play("Run");
+		}
+	}
+}
