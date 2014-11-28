44c757140a3059bd4069d8a1fcdd0e4dc5826518
EpicAglet <DamienCrielaard@gmail.com>
2014-11-28 14:15:35 +0100

    Animations

--- Unity project/Assets/PlayerAnimationManager.cs
+++ Unity project/Assets/PlayerAnimationManager.cs
@@ -4,23 +4,30 @@ using System.Collections;
 public class PlayerAnimationManager : MonoBehaviour {
 
 	public Transform playerMesh;
+	public float epsilon = 0.01f;
 
 	private Animation anim;
+	private CharacterController controller;
 
 	// Use this for initialization
 	void Start () {
 		anim = playerMesh.GetComponent<Animation>();
 		anim["Run"].speed = 3;
+		controller = GetComponent(typeof(CharacterController)) as CharacterController;
+		anim.Play("IdleGun");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (anim!=null){
-			if(Input.GetKeyDown(KeyCode.Keypad9)){
-				anim.Play("Idle");
-			}
-			if(Input.GetKeyDown(KeyCode.Keypad8))
-				anim.Play("Run");
+		if(controller.velocity.y > epsilon){
+			anim.Play("Jump");
+		} else if(controller.velocity.magnitude > 1){
+			anim.Play("Run");
+		} else {
+			anim.Play("IdleGun");
 		}
 	}
+
+
+
 }
---
+++ Unity project/Assets/RuntimeParenting.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RuntimeParenting : MonoBehaviour {
+
+	public Transform newParent;
+
+	// Use this for initialization
+	void Start () {
+		transform.parent = newParent;
+	}
+
+}
