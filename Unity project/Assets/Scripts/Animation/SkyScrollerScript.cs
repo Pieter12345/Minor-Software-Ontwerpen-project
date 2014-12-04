bbd9fcd981b59cf15c9d446c264c12fe0fe9c9a6
Jeroen0m <jeroen.methorst@ziggo.nl>
2014-12-04 12:45:44 +0100

    Added Sky
    - Sky texture
    - Sky Prefab
    - Sky Scroller Script

---
+++ Unity project/Assets/Scripts/Animation/SkyScrollerScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyScrollerScript : MonoBehaviour {
+    public Vector2 Scroll = new Vector2(0.05f, 0.05f);
+    Vector2 Offset = new Vector2(0f, 0f);
+
+    void Update()
+    {
+        Offset += Scroll * Time.deltaTime;
+        renderer.material.SetTextureOffset("_MainTex", Offset);
+    }
+}
