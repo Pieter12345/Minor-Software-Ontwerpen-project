da7fff327d14565456b1e27693ccc21643746c88
EpicAglet <DamienCrielaard@gmail.com>
2014-12-16 11:51:37 +0100

    Added healthbar

---
+++ Unity project/Assets/Scripts/UI/Healthbar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Healthbar : MonoBehaviour {
+
+	public Transform player;
+	private Health playerHP;
+	
+	private Slider slider;
+	
+	// Use this for initialization
+	void Start () {
+		if(player!=null)
+			playerHP = player.GetComponent(typeof(Health)) as Health;
+		slider = GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(playerHP != null){
+			slider.value = playerHP.HP/playerHP.maxHP;
+		}
+	}
+}
