a33049b6e46f002ba161431dc130dd7172646350
EpicAglet <DamienCrielaard@gmail.com>
2014-12-16 10:39:34 +0100

    Included simple menus

---
+++ Unity project/Assets/Scripts/UI/MenuButtons.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtons : MonoBehaviour {
+
+	public void HowTo(){
+		Debug.Log ("Showing How To");
+		Application.LoadLevel("HowTo");
+	}
+
+	public void ToMainMenu(){
+		Debug.Log ("Loading Main Menu");
+		Application.LoadLevel("MainMenu");
+	}
+
+	public void RetryLevel(){
+		Debug.Log ("Loading Level");
+		Application.LoadLevel("Level");
+	}
+
+	public void QuitGame(){
+		Debug.Log ("Exiting game");
+		Application.Quit();
+	}
+}
