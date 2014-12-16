58efeb6cf6e7b22b4499c66d2d822ead9bf91814
EpicAglet <DamienCrielaard@gmail.com>
2014-12-16 12:30:50 +0100

    Enemies counter

---
+++ Unity project/Assets/Scripts/UI/EnemiesCounter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EnemiesCounter : MonoBehaviour {
+
+	private Text t;
+	
+	// Use this for initialization
+	void Start () {
+		t = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		t.text = "Enemies Remaining: " + EnemyController.getEnemiesLeft();
+	}
+}
