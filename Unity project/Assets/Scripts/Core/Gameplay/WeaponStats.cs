0dacc7d8ae3fbea16a525db6d8524dca2f5a945f
EpicAglet <DamienCrielaard@gmail.com>
2014-12-17 15:26:56 +0100

    Added RPG dummy

--- Unity project/Assets/Scripts/Core/Gameplay/WeaponStats.cs
+++ Unity project/Assets/Scripts/Core/Gameplay/WeaponStats.cs
@@ -5,7 +5,8 @@ public enum WeaponStats {
 
 	PISTOL,
 	RIFLE,
-	SHOTGUN
+	SHOTGUN,
+	RPG
 
 }
 public static class WeaponStatsExtension{
@@ -16,6 +17,7 @@ public static class WeaponStatsExtension{
 		case WeaponStats.PISTOL:	foo = 10;	break;
 		case WeaponStats.RIFLE:		foo = 20;	break;
 		case WeaponStats.SHOTGUN:	foo = 2;	break;
+		case WeaponStats.RPG:		foo = 1;	break;
 		}
 		return foo;
 	}
@@ -26,6 +28,7 @@ public static class WeaponStatsExtension{
 		case WeaponStats.PISTOL:	foo = AmmoTypes.PISTOL_BULLET;	break;
 		case WeaponStats.RIFLE:		foo = AmmoTypes.RIFLE_BULLET;	break;
 		case WeaponStats.SHOTGUN:	foo = AmmoTypes.SHELL;			break;
+		case WeaponStats.RPG:		foo = AmmoTypes.ROCKET;			break;
 		}
 		return foo;
 	}
@@ -36,6 +39,7 @@ public static class WeaponStatsExtension{
 		case WeaponStats.PISTOL:	foo = 1f;	break;
 		case WeaponStats.RIFLE:		foo = 1.2f;	break;
 		case WeaponStats.SHOTGUN:	foo = 10f;	break;
+		case WeaponStats.RPG:		foo = 50f;	break;
 		}
 		return foo;
 	}
@@ -46,6 +50,7 @@ public static class WeaponStatsExtension{
 		case WeaponStats.PISTOL:	foo = 0.2f;	break;
 		case WeaponStats.RIFLE:		foo = 0.2f;	break;
 		case WeaponStats.SHOTGUN:	foo = 1f;	break;
+		case WeaponStats.RPG:		foo = 3f;	break;
 		}
 		return foo;
 	}
@@ -56,6 +61,7 @@ public static class WeaponStatsExtension{
 		case WeaponStats.PISTOL:	foo = 1f;	break;
 		case WeaponStats.RIFLE:		foo = 0.2f;	break;
 		case WeaponStats.SHOTGUN:	foo = 1f;	break;
+		case WeaponStats.RPG:		foo = 3f;	break;
 		}
 		return foo;
 	}
@@ -66,6 +72,7 @@ public static class WeaponStatsExtension{
 		case WeaponStats.PISTOL:	foo = 0.5f;	break;
 		case WeaponStats.RIFLE:		foo = 1f;	break;
 		case WeaponStats.SHOTGUN:	foo = 3f;	break;
+		case WeaponStats.RPG:		foo = 5f;	break;
 		}
 		return foo;
 	}
---
+++ Unity project/Assets/Scripts/WeaponTypes/RPG.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RPG : Weapon {
+
+	public override void Fire(Vector3 from, Vector3 to){
+
+	}
+}
