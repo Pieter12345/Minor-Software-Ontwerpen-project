e0b592c1f5e70152ecd9adb098bc8af6977d31fd
EpicAglet <DamienCrielaard@gmail.com>
2014-12-17 14:27:56 +0100

    Player regens health

--- Unity project/Assets/Scripts/Core/Gameplay/PlayerHealth.cs
+++ Unity project/Assets/Scripts/Core/Gameplay/PlayerHealth.cs
@@ -7,12 +7,15 @@ public class PlayerHealth : Health {
 	[Range(0.0f, 1.0f)]
 	public float regeneratingFraction = 0.2f;
 	public float regenSpeed = 1.0f;
-	public float regenCooldown = 10f;
+	public float regenCooldown = 2f;
 
 	private float regenTo = 0f;
 	private float timeLastHit;	
+	private float epsilon = 0.1f;
 
-	
+	void Start(){
+		regenTo = maxHP;
+	}
 
 	protected override void OnDeath(){
 		Debug.Log("YOU JUST DIED MADAFAKA!!!");
@@ -21,19 +24,21 @@ public class PlayerHealth : Health {
 	}
 
 	protected override void OnDamage(float amount){
-		regenTo = (hp/maxHP + regeneratingFraction > 1f) ? hp + regeneratingFraction*maxHP : maxHP;
+		regenTo = (hp/maxHP + regeneratingFraction < 1f) ? (hp + regeneratingFraction*maxHP) : maxHP;
 		timeLastHit = Time.time;
 	}
 
 	protected override void OnDamage(){
-		regenTo = (hp/maxHP + regeneratingFraction > 1f) ? hp + regeneratingFraction*maxHP : maxHP;
+		regenTo = (hp/maxHP + regeneratingFraction < 1f) ? (hp + regeneratingFraction*maxHP) : maxHP;
 		timeLastHit = Time.time;
 	}
 
 	protected override void OnRegen(){
 		if(playerCanRegen){
+			if(Mathf.Abs(hp-regenTo) < epsilon)
+				hp = regenTo;
 			if(Time.time - timeLastHit > regenCooldown)
-				Mathf.Lerp(hp, regenTo, regenSpeed*Time.deltaTime);
+				hp = Mathf.Lerp(hp, regenTo, regenSpeed*Time.deltaTime);
 		}
 	}
 
