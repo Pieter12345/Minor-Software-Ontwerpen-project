41223f852014de1265d02138c392aa189fa2a465
EpicAglet <DamienCrielaard@gmail.com>
2014-12-17 15:48:38 +0100

    RPG maar doe het nie

--- Unity project/Assets/Scripts/WeaponTypes/RPG.cs
+++ Unity project/Assets/Scripts/WeaponTypes/RPG.cs
@@ -3,7 +3,14 @@ using System.Collections;
 
 public class RPG : Weapon {
 
+	public Transform grenade;
+
 	public override void Fire(Vector3 from, Vector3 to){
+		if(grenade == null){
+			Debug.LogError("RPG has no grenade!");
+			return;
+		}
+		grenade.GetComponent<RPGGrenade>().Fire(to);
 
 	}
 }
