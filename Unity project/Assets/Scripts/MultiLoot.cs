93c2b32c56162adf6f02723cc29a200c92464cde
EpicAglet <DamienCrielaard@gmail.com>
2014-12-18 12:50:31 +0100

    New spawn system draft

---
+++ Unity project/Assets/Scripts/MultiLoot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiLoot : MonoBehaviour {
+
+	private GameObject spawner;
+	private LootSpawner ls;
+
+	public GameObject[] spawnable;
+
+	// Use this for initialization
+	void Start () {
+		spawner = GameObject.FindGameObjectWithTag("PickupSpawner");
+		ls = spawner.GetComponent<LootSpawner>();
+	}
+	
+	// Update is called once per frame
+	void OnDisable () {
+		if(ls==null){
+			Debug.LogWarning("NO SPAWNER FOUND!");
+			return;
+		}
+		int spawnindex = Mathf.FloorToInt(Random.Range(0,spawnable.Length-1));
+		ls.Spawn(spawnable[spawnindex], transform.position, Quaternion.identity);
+	}
+}
