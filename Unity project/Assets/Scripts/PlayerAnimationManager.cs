4e5667ec9aa672dad49dc19a274dcc1ac5d67bdb
EpicAglet <DamienCrielaard@gmail.com>
2014-12-19 16:40:30 +0100

    GITHUB WERKT NIE
    EN NIET ZON KLEIN BEETJE OOK

--- Unity project/Assets/Scripts/PlayerAnimationManager.cs
+++ Unity project/Assets/Scripts/PlayerAnimationManager.cs
@@ -7,25 +7,43 @@ public class PlayerAnimationManager : MonoBehaviour {
 	public Transform playerMesh;
 	public float epsilon = 0.01f;
 
+	public GameObject weapons;
+	private WeaponController wContr;
+
 	private Animation anim;
 	private CharacterController controller;
 
 	// Use this for initialization
 	void Start () {
 		anim = playerMesh.GetComponent<Animation>();
-		anim["Run"].speed = 3;
+		anim["ColtLopen"].speed = 3;
+		anim["ShotgunLopen"].speed = 3;
 		controller = GetComponent(typeof(CharacterController)) as CharacterController;
-		anim.Play("IdleGun");
+		wContr = weapons.GetComponent(typeof(WeaponController)) as WeaponController;
+		anim.Play("ColtIdle");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(controller == null){
+			controller = GetComponent(typeof(CharacterController)) as CharacterController;
+			return;
+		}
 		if(controller.velocity.y > epsilon){
-			anim.CrossFade("Jump");
+			anim.CrossFade("Springen");
 		} else if(controller.velocity.magnitude > 1){
-			anim.CrossFade("Run");
+			if(wContr.HasWeapon)
+				if(wContr.SelectedWeaponType != WeaponStats.RIFLE && wContr.SelectedWeaponType != WeaponStats.SHOTGUN)
+					anim.CrossFade("ColtLopen");
+				else
+					anim.CrossFade("ShotgunLopen");
+			else
+				anim.CrossFade("Rennen");
 		} else {
-			anim.CrossFade("IdleGun");
+			if(wContr.HasWeapon)
+				anim.CrossFade("ColtIdle");
+			else
+				anim.CrossFade("Wachten");
 		}
 	}
 
