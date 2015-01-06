f2277d41e3e3a927dcfadc311d80d79694c8cf1a
EpicAglet <DamienCrielaard@gmail.com>
2015-01-06 11:54:16 +0100

    100th commit!!!!!!!! Oh and also added gun icons
    Gee, I don't know what to say... I'd like to thank all of my amazing
    fanbase for their support. I'd also like to thank the rest of the team,
    without them all of this would not have been possible.
    
    An icon of the selected gun is now displayed next to ammo counter

---
+++ Unity project/Assets/GunIconHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Image))]
+public class GunIconHandler : MonoBehaviour {
+
+	public Transform weaponController;
+	private WeaponController wContr;
+
+	private Image icon;
+	public Sprite[] icons;
+
+	void Start(){
+		wContr = weaponController.GetComponent<WeaponController>() as WeaponController;
+		icon = GetComponent<Image>();
+		icon.enabled = wContr.HasWeapon;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(icons.Length < wContr.weapons.Length){
+			Debug.LogWarning("Not enough sprites to show icon in GunIconHandler");
+			return;
+		}
+
+
+		if(wContr != null){
+			icon.enabled = wContr.HasWeapon;
+			icon.sprite = icons[wContr.SelectedWeapon];
+		}
+	}
+}
