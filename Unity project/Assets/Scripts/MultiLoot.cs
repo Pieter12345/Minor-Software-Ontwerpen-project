b9b5f1d8a9a17c7e9cef34503f7391861ca667ce
EpicAglet <DamienCrielaard@gmail.com>
2015-01-06 09:45:44 +0100

    Added support for individual probabilities on loot

--- Unity project/Assets/Scripts/MultiLoot.cs
+++ Unity project/Assets/Scripts/MultiLoot.cs
@@ -5,14 +5,22 @@ public class MultiLoot : MonoBehaviour {
 
 	private GameObject spawner;
 	private LootSpawner ls;
+	private float cumulativeProb;
+
 	[Range(0.0f, 1.0f)]
 	public float masterProbability = 0.4f;
-	public GameObject[] spawnable;
+	public LootAble[] lootAble;
 
 	// Use this for initialization
 	void Start () {
 		spawner = GameObject.FindGameObjectWithTag("PickupSpawner");
 		ls = spawner.GetComponent<LootSpawner>();
+		cumulativeProb = 0;
+		if(lootAble.Length > 0){
+			foreach(LootAble l in lootAble){
+				cumulativeProb += l.probability;
+			}
+		}
 	}
 	
 	// Update is called once per frame
@@ -22,8 +30,21 @@ public class MultiLoot : MonoBehaviour {
 			return;
 		}
 		if(Random.Range(0f, 1f) < masterProbability){
-			int spawnindex = Mathf.FloorToInt(Random.Range(0,spawnable.Length));
-			ls.Spawn(spawnable[spawnindex], transform.position, Quaternion.identity);
+//			int spawnindex = Mathf.FloorToInt(Random.Range(0,lootAble.Length));
+			ls.Spawn(GetToSpawn(), transform.position, Quaternion.identity);
 		}
+	}
+
+	GameObject GetToSpawn(){
+		float probSum = 0f;
+		float rnd = Random.Range(0f, cumulativeProb);
+		if(lootAble.Length > 0){
+			foreach(LootAble l in lootAble){
+				probSum += l.probability;
+				if (rnd < probSum)
+					return l.pickUp;
+			}
+		}
+		return lootAble[lootAble.Length-1].pickUp;
 	}
 }
