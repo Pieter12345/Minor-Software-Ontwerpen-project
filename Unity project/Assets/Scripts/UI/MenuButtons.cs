9041465fdfdd18b9aa9a83dc184c0cab9fbd4565
Jeroen0m <jeroen.methorst@ziggo.nl>
2015-01-07 15:11:14 +0100

    Finished Splash screen. Added Menu fading

--- Unity project/Assets/Scripts/UI/MenuButtons.cs
+++ Unity project/Assets/Scripts/UI/MenuButtons.cs
@@ -1,26 +1,80 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MenuButtons : MonoBehaviour {
+public class MenuButtons : MonoBehaviour
+{
+    public UnityEngine.UI.RawImage Fade;
 
-	public void HowTo(){
-		Debug.Log ("Showing How To");
-		Application.LoadLevel("HowTo");
-	}
+    //Fade into menu on load//
+    void Start()
+    {
+        Fade.CrossFadeAlpha(0f, 1f, true);
+        //Invoke("FadeDisable", 1f);
+    }
+    
+    //(dep)
+    void FadeDisable()
+    {
+        Fade.gameObject.SetActive(false);
+    }
+    //////////////////////////
 
-	public void ToMainMenu(){
-		Debug.Log ("Loading Main Menu");
-		Application.LoadLevel("MainMenu");
-	}
+    //Functions corresponding to Menu Buttons//
+    public void HowTo()
+    {
+        LoadScene("LoadHowTo");
+    }
 
-	public void RetryLevel(){
-		Debug.Log ("Loading Level");
-		Screen.lockCursor = true;
-		Application.LoadLevel("Level");
-	}
 
-	public void QuitGame(){
-		Debug.Log ("Exiting game");
-		Application.Quit();
-	}
+    public void ToMainMenu()
+    {
+        LoadScene("LoadMainMenu");
+    }
+
+    public void RetryLevel()
+    {
+        LoadScene("LoadRetryLevel");
+
+    }
+
+    public void QuitGame()
+    {
+        LoadScene("LoadQuitGame");
+
+    }
+    ///////////////////////////////////////////
+
+    //Code corresponding to Menu Buttons//
+    void LoadHowTo()
+    {
+        Debug.Log("Showing How To");
+        Application.LoadLevel("HowTo");
+    }
+
+    void LoadMainMenu()
+    {
+        Debug.Log("Loading Main Menu");
+        Application.LoadLevel("MainMenu");
+    }
+
+    void LoadRetryLevel()
+    {
+        Debug.Log("Loading Level");
+        Screen.lockCursor = true;
+        Application.LoadLevel("Level");
+    }
+
+    void LoadQuitGame()
+    {
+        Debug.Log("Exiting game");
+        Application.Quit();
+    }
+    //////////////////////////////////////
+
+    //Fades screen to black in 1 second, then calls the function
+    void LoadScene(string function)
+    {
+        Fade.CrossFadeAlpha(1f, 1f, true);
+        Invoke(function, 1f);
+    }
 }
