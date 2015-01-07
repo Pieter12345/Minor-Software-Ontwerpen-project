a847a6b39bfac437cf99a462e7f58e5bb28ef3a0
EpicAglet <DamienCrielaard@gmail.com>
2015-01-07 16:26:46 +0100

    Added message on flag damage

--- Unity project/Assets/Scripts/Core/Gameplay/FlagHealth.cs
+++ Unity project/Assets/Scripts/Core/Gameplay/FlagHealth.cs
@@ -3,6 +3,20 @@ using System.Collections;
 
 public class FlagHealth : Health {	
 
+	public GameObject underAttackMessage;
+
+	private UnderAttackMessage uam;
+
+	void Start() {
+		if(underAttackMessage != null)
+			uam = underAttackMessage.GetComponent<UnderAttackMessage> ();
+	}
+
+	protected override void OnDamage() {
+		if(uam!= null)
+			uam.Show();
+	}
+
 	protected override void OnDeath(bool isHeadshot){
 		Debug.Log("Flag got killed");
 		EndGame();
---
+++ Unity project/Assets/Scripts/UI/UnderAttackMessage.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Text))]
+public class UnderAttackMessage : MonoBehaviour {
+
+	public float fadeDelayTime = 2f;
+	public float fadeSpeed = 5f;
+	
+	private Text txt;
+	private float timeLastHit;
+
+	public float Alpha { 
+		get {
+			if(txt != null)
+				return txt.color.a;
+			else
+				return 0f;
+		}
+		set {
+			if(txt != null)
+				txt.color = new Color(txt.color.r, txt.color.g, txt.color.b, value);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		txt = GetComponent<Text>();
+		Alpha = 0f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Time.time - timeLastHit > fadeDelayTime){
+			Alpha -= fadeSpeed;
+		}
+		if(Alpha < 0f)
+			Alpha = 0f;
+	}
+
+	public void Show(){
+		timeLastHit = Time.time;
+		Alpha = 1f;
+	}
+
+}
