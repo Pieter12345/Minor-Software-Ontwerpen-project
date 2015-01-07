8888f3cd02d85eb970f2e5884aefe03acde9670d
EpicAglet <DamienCrielaard@gmail.com>
2015-01-07 15:54:32 +0100

    Added "Reload you weapon" text

---
+++ Unity project/Assets/Scripts/UI/ReloadText.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Text))]
+public class ReloadText : MonoBehaviour {
+
+	public GameObject weapons;
+
+	private Text txt;
+	private WeaponController wContr;
+
+	// Use this for initialization
+	void Start () {
+		txt = GetComponent<Text>();
+		txt.enabled = false;
+		wContr = weapons.GetComponent<WeaponController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(wContr == null || weapons == null) { return; }
+		txt.enabled = (wContr.HasWeapon && wContr.SelectedWeaponInClip < 1 && wContr.SelectedWeaponInPool > 0);
+
+	}
+}
