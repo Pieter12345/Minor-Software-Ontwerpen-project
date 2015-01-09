e3a524ed836b8fe7199e8342db98a53eeb602bf6
EpicAglet <DamienCrielaard@gmail.com>
2015-01-09 15:48:30 +0100

    Added a healthPickup

---
+++ Unity project/Assets/Scripts/HealthPickUp.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPickUp : MonoBehaviour {
+
+	public float amountToHeal = 10f;
+
+	void OnTriggerEnter (Collider other) {
+		if(other.tag.Equals("Player")){
+			other.transform.parent.GetComponent<PlayerHealth>().Heal(amountToHeal);
+		}
+	}
+}
