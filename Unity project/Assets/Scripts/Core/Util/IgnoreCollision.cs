9f34e01e1134780bcb88828fa0a4a66744ae3bf1
EpicAglet <DamienCrielaard@gmail.com>
2015-01-16 14:58:47 +0100

    Player and enemies now don't collide with shells

---
+++ Unity project/Assets/Scripts/Core/Util/IgnoreCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgnoreCollision : MonoBehaviour {
+
+	public GameObject[] toIgnore;
+
+	void OnEnable(){
+		foreach(GameObject o in toIgnore){
+			Physics.IgnoreCollision(collider, o.collider);
+		}
+	}
+
+}
---
+++ Unity project/Assets/Scripts/Core/Util/IgnoreCollisionByTag.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgnoreCollisionByTag : MonoBehaviour {
+
+	public string[] toIgnore;
+
+	// Use this for initialization
+	void OnEnable () {
+		if (collider == null)
+			return;
+
+		if(toIgnore.Length > 0){
+			foreach(string s in toIgnore){
+				GameObject[] gos = GameObject.FindGameObjectsWithTag(s);
+				if(gos.Length>0){
+					foreach(GameObject o in gos){
+						Collider col = o.collider;
+						if(col != null)
+							Physics.IgnoreCollision(col, collider);
+					}
+				}
+			}
+		}
+	}
+
+}
