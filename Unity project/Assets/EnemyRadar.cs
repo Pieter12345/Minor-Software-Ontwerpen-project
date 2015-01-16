2b4da002f5afd5f71567b8d126cb313dcca72534
Pietje <p.j.s.kools@student.tudelft.nl>
2015-01-16 17:39:08 +0100

    IgnoreMe

--- Unity project/Assets/EnemyRadar.cs
+++ Unity project/Assets/EnemyRadar.cs
@@ -4,13 +4,12 @@ using System.Collections;
 
 public class EnemyRadar : MonoBehaviour {
 
-	public Texture2D enemyRadarDotTexture;
-	public Texture2D radarTexture;
+	public Sprite enemyRadarDotSprite;
+	public Sprite radarSprite;
+
 	public int radarSize = 100;
 
 	public GameObject radarPanel;
-
-	public Sprite enemyRadarSprite;
 
 //	private Sprite enemyRadarSprite;
 
@@ -18,12 +17,14 @@ public class EnemyRadar : MonoBehaviour {
 	void Start () {
 //		this.enemyRadarSprite = Sprite.Create(this.radarTexture, new Rect(0f, 0f, this.radarSize >> 1, this.radarSize >> 1), new Vector2(this.radarSize >> 1, this.radarSize >> 1));
 
-		GameObject radarObject = new GameObject();
-		radarObject.AddComponent("Image");
-		radarObject.GetComponent<Image>().sprite = this.enemyRadarSprite;
-		radarObject.transform.SetParent(this.radarPanel.transform);
-		radarObject.transform.localPosition = new Vector3(50f, 50f, 0f);
-		radarObject.transform.localScale = new Vector3(0.1f, 0.1f, 1f);
+		this.createRadar();
+
+//		GameObject radarObject = new GameObject();
+//		radarObject.AddComponent("Image");
+//		radarObject.GetComponent<Image>().sprite = this.enemyRadarDotSprite;
+//		radarObject.transform.SetParent(this.radarPanel.transform);
+//		radarObject.transform.localPosition = new Vector3(50f, 50f, 0f);
+//		radarObject.transform.localScale = new Vector3(0.1f, 0.1f, 1f);
 
 	}
 	
@@ -31,4 +32,28 @@ public class EnemyRadar : MonoBehaviour {
 	void Update () {
 //		GUI.DrawTexture(new Rect(0f, 0f, this.radarSize >> 1, this.radarSize >> 1), this.radarTexture);
 	}
+
+	// createRadar method.
+	// Draws the radar to the screen.
+	public void createRadar() {
+		GameObject radarObject = new GameObject();
+		radarObject.AddComponent("Image");
+		radarObject.GetComponent<Image>().sprite = this.radarSprite;
+		radarObject.transform.SetParent(this.radarPanel.transform);
+		radarObject.transform.localPosition = new Vector3(this.radarSize >> 1, this.radarSize >> 1, 0f); // Set radar to the middle of the panel.
+		// TODO - Scale image to desired radarSize based on sprite size?
+
+	}
+
+	// addEnemyDot method.
+	// Draws enemy dots to the radar.
+	public void addEnemyDot(Vector3 pos) {
+		GameObject enemyDotObject = new GameObject();
+		enemyDotObject.AddComponent("Image");
+		enemyDotObject.GetComponent<Image>().sprite = this.enemyRadarDotSprite;
+		enemyDotObject.transform.SetParent(this.radarPanel.transform);
+		enemyDotObject.transform.localScale = new Vector3(0.1f, 0.1f, 1f); // TODO - Resize based on sprite size?
+		enemyDotObject.transform.localPosition = pos;
+	}
+
 }
