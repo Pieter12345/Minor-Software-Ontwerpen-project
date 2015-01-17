bcadefebf9b61787048db7846c903f6e09cda730
Pietje <p.j.s.kools@student.tudelft.nl>
2015-01-17 20:14:01 +0100

    Added some code for the enemy radar (unfinished).

--- Unity project/Assets/EnemyRadar.cs
+++ Unity project/Assets/EnemyRadar.cs
@@ -11,12 +11,25 @@ public class EnemyRadar : MonoBehaviour {
 
 	public GameObject radarPanel;
 
+	private GameObject[] enemyDots;
+
 //	private Sprite enemyRadarSprite;
 
 	// Use this for initialization.
 	void Start () {
 //		this.enemyRadarSprite = Sprite.Create(this.radarTexture, new Rect(0f, 0f, this.radarSize >> 1, this.radarSize >> 1), new Vector2(this.radarSize >> 1, this.radarSize >> 1));
 
+		// Create 1000 dots for the minimap. This should be enough.
+		this.enemyDots = new GameObject[1000];
+		for(int i = 0; i < enemyDots.Length; i++) {
+			this.enemyDots[i].AddComponent("Image");
+			this.enemyDots[i].GetComponent<Image>().sprite = this.enemyRadarDotSprite;
+			this.enemyDots[i].transform.SetParent(this.radarPanel.transform);
+			this.enemyDots[i].transform.localScale = new Vector3(0.1f, 0.1f, 1f);
+			this.enemyDots[i].SetActive(false);
+		}
+
+		// Create and show the radar.
 		this.createRadar();
 
 //		GameObject radarObject = new GameObject();
@@ -30,6 +43,23 @@ public class EnemyRadar : MonoBehaviour {
 	
 	// Update is called once per frame.
 	void Update () {
+
+		// Get all enemy objects.
+		GameObject[] enemyObjects = EnemyController.getEnemyObjects();
+
+		// Set position and rotation of enemyDots.
+		for(int i = 0; i < enemyObjects.Length; i++) {
+//			this.enemyDots[i].transform.localPosition = ... // TODO - set position and rotation here.
+			this.enemyDots[i].SetActive(true);
+		}
+
+		// Disable any remaining enemyDots.
+		int j = enemyObjects.Length;
+		while(j < enemyDots.Length && this.enemyDots[j].activeInHierarchy) {
+			this.enemyDots[j].SetActive(false);
+		}
+
+
 //		GUI.DrawTexture(new Rect(0f, 0f, this.radarSize >> 1, this.radarSize >> 1), this.radarTexture);
 	}
 
